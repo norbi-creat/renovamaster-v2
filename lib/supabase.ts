b880d3777b35e7d78a845f0493ad0094dd501766
norbi-creat <miski.norbert@gmail.com>
2026-01-16 21:50:33 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.https://futettepsqkyxgmerjmn.supabase.co!,
+  process.env.sb_publishable_6j4nvZYvzgG0vETAK-ZQVA_Ii2c7sg0!
 );
